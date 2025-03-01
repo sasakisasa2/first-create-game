4029fc8add3ba8aecde5924d5d1ded007b97abcf
sasakisasa2 <nanami.ahiru@gmail.com>
2025-03-02 01:47:55 +0900

    保存用の変数と設定用の関数の宣言

--- MainProject/Classes/collision/CollisionInfo/ColisionInfo.h
+++ MainProject/Classes/collision/CollisionInfo/ColisionInfo.h
@@ -7,7 +7,15 @@ using namespace DirectX;
 class CollisionInfo
 {
 private:
-
+	SimpleMath::Vector2 squareCorner[4];
 public:
+	/// <summary>
+	/// “–‚½‚è”»’è‚Ìˆ×‚ÌˆÊ’u•Û‘¶—p
+	/// </summary>
+	/// <param name="position">ˆÊ’u</param>
+	/// <param name="vector">Œü‚©‚¤•ûŒü</param>
+	void SetSquareCorner(SimpleMath::Vector2 position, SimpleMath::Vector2 vector,float angle)
+	{
 
+	}
 };
