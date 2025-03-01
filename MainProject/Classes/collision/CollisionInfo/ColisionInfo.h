4b6e9b4c48b4c704bb0be892f0ea8abf09d6295c
sasakisasa2 <nanami.ahiru@gmail.com>
2025-03-02 02:30:04 +0900

    定数の宣言

--- MainProject/Classes/collision/CollisionInfo/ColisionInfo.h
+++ MainProject/Classes/collision/CollisionInfo/ColisionInfo.h
@@ -3,19 +3,28 @@
 #include"..\MainProject\Base\pch.h"
 
 using namespace DirectX;
+using namespace SimpleMath;
 
 class CollisionInfo
 {
 private:
-	SimpleMath::Vector2 squareCorner[4];
+	:Vector2 squareCorner[4];
 public:
 	/// <summary>
 	/// 当たり判定の為の位置保存用
 	/// </summary>
 	/// <param name="position">位置</param>
 	/// <param name="vector">向かう方向</param>
-	void SetSquareCorner(SimpleMath::Vector2 position, SimpleMath::Vector2 vector,float angle)
+	void SetSquareCorner(Vector2 position, Vector2 vector, Vector2 size, float angle, float speed)
 	{
+		//計算用定数
+		const float   NINETY = 90;
+		const float   MINUS  = -1;
+		const Vector2 SIZE_HALF = size / 2;
 
+		//サイズから逆算した角への角度
+		const Vector2 CORNER_ANGLE = CF::RectangleCornerAngle(SIZE_HALF);
+		//角への距離
+		const float RADIUS = CF::Distance(Vector2(SIZE_HALF.x,0.0f), Vector2(0.0f,SIZE_HALF.y));
 	}
 };
