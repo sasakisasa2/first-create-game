832d1afa0c12e4fe74e1784bd48640905a910eb6
sasakisasa2 <nanami.ahiru@gmail.com>
2025-03-03 13:45:08 +0900

    PlantクラスにPlantManagerクラスを継承

--- MainProject/Classes/Enamy/Plant/Plant.h
+++ MainProject/Classes/Enamy/Plant/Plant.h
@@ -1,16 +1,19 @@
 #pragma once
 #include"..\MainProject\Base\pch.h"
 #include"..\MainProject\Base\dxtk.h"
+#include"..\MainProject\Classes\Enamy\Plant\PlantManager\PlantManager.h"
 #include"..\MainProject\Classes\Enamy\Plant\PlantAnimation\PlantAnimation.h"
 #include"..\MainProject\Classes\Enamy\Plant\PlantAttack\PlantAttack.h"
 #include"..\MainProject\Classes\Enamy\Plant\PlantSpawn\PlantSpawn.h"
 
-class Plant 
+class Plant:public PlantManager
 {
 private:
 	PlantSpawn     spawn;
 	PlantAnimation animation;
 	PlantAttack    attack;
+
+
 public:
 	void Load(DirectXTK::Sprite& setSprite,DirectXTK::Sprite& setAttackSprite);
 	void Initialize();
