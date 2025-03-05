ab165ee70a232487285a1630d430538a0982e631
sasakisasa2 <nanami.ahiru@gmail.com>
2025-03-06 02:43:11 +0900

    サイズの設定修正、コメントアウトの追加

--- MainProject/Classes/collision/CollisionDetection/CollisionDetection.h
+++ MainProject/Classes/collision/CollisionDetection/CollisionDetection.h
@@ -20,11 +20,23 @@ public:
 		//•ª—£²İ’è
 		Vector2 axis[4]
 		{ 
-			CF::ChangeAngleToVector(objectOne.GetAngle()),
-			CF::ChangeAngleToVector(objectOne.GetAngle() + NINTY),
-			CF::ChangeAngleToVector(objectTwo.GetAngle()),
-			CF::ChangeAngleToVector(objectTwo.GetAngle() + NINTY),
+			CF::ChangeAngleToVector(objectOne.GetAngle()),        //object1‚ÌŒü‚¢‚Ä‚é•ûŒü
+			CF::ChangeAngleToVector(objectOne.GetAngle() + NINTY),//object1‚ÌŒü‚¢‚Ä‚é•ûŒü‚©‚ç‚’¼•ûŒü
+			CF::ChangeAngleToVector(objectTwo.GetAngle()),        //object2‚ÌŒü‚¢‚Ä‚é•ûŒü
+			CF::ChangeAngleToVector(objectTwo.GetAngle() + NINTY) //object2‚ÌŒü‚¢‚Ä‚é•ûŒü‚©‚ç‚’¼•ûŒü
 		};
-		Vector2 sizeHalf[2]{ objectOne.GetSize() / 2.0f,objectTwo.GetSize() / 2.0f };
+
+		Vector2 sizeHalf[2]
+		{ 
+			//x.Œü‚¢‚Ä‚é•ûŒü‚Ì•Ó‚Ì’·‚³||y.Œü‚¢‚Ä‚é•ûŒü‚©‚ç‚’¼‚Ì•Ó‚Ì’·‚³
+			Vector2(
+				CF::Distance(objectOne.GetSquareCorner(0),objectOne.GetSquareCorner(2)),
+				CF::Distance(objectOne.GetSquareCorner(1),objectOne.GetSquareCorner(3))
+			),
+			Vector2(
+				CF::Distance(objectTwo.GetSquareCorner(0),objectTwo.GetSquareCorner(2)),
+				CF::Distance(objectTwo.GetSquareCorner(1),objectTwo.GetSquareCorner(3))
+			)
+		};
 	}
 };
