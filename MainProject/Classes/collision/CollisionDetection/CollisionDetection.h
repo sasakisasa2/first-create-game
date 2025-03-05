edb24da61f6703cbc178ffebc8053bd2f380cde0
sasakisasa2 <nanami.ahiru@gmail.com>
2025-03-06 01:55:57 +0900

    分離軸の設定(オブジェクト１の向いてる方向）

--- MainProject/Classes/collision/CollisionDetection/CollisionDetection.h
+++ MainProject/Classes/collision/CollisionDetection/CollisionDetection.h
@@ -2,9 +2,19 @@
 #include"..\MainProject\Base\pch.h"
 #include"..\MainProject\Base\dxtk.h"
 
+#include"..\CollisionInfo\CollisionInfo.h"
+class CollisionInfo;
+
 class CD
 {
 private:
 
 public:
+	bool CollisionDetection(CollisionInfo& objectOne, CollisionInfo& objectTwo)
+	{
+		//オブジェクト１の向きを分離軸として設定
+		bool detection = false;
+		Vector2 axis = CF::ChangeAngleToVector(objectOne.GetAngle());
+	}
+
 };
